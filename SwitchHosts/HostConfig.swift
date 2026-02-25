529a0bea50c7783d2a83046f27e91b8a3e4e5ab0
iappapp <iadore@live.cn>
2026-02-25 10:58:12 +0800

    first commit

--- SwitchHosts/HostConfig.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct HostConfig: Identifiable, Hashable {
-    let id = UUID()
-    var name: String
-    var content: String
-    var isActive: Bool
-    var isSystem: Bool = false
-}
