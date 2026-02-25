67e5662d4d04cbe6cf471f5378731dd856cee712
iappapp <iadore@live.cn>
2026-02-25 09:39:29 +0800

    Initial Commit

---
+++ SwitchHosts/SwitchHostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchHostsApp.swift
+//  SwitchHosts
+//
+//  Created by mac on 2026/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwitchHostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwitchHostsTests/SwitchHostsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchHostsTests.swift
+//  SwitchHostsTests
+//
+//  Created by mac on 2026/2/25.
+//
+
+import Testing
+@testable import SwitchHosts
+
+struct SwitchHostsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
