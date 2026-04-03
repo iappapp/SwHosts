6035c17f180718725735b98791918749d87a7fbe
iappapp <iadore@live.cn>
2026-04-03 09:18:45 +0800

    update readme

--- SwitchHosts/SwitchHostsApp.swift
+++ SwitchHosts/SwitchHostsApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct SwitchHostsApp: App {
     var body: some Scene {
-        WindowGroup {
+        Window("Main", id: "main") {
             ContentView()
         }
     }
